52a9924a6fdf78c4882a2559aa7cd95a5e85a24d
rishiA <rishiapril2000@gmail.com>
2023-10-16 09:44:29 -0700

    vgrf regression using MLP

--- RegressionLossEvaluator.py
+++ RegressionLossEvaluator.py
@@ -2,6 +2,7 @@ import torch
 from AddBiomechanicsDataset import OutputDataKeys
 from typing import Dict
 import numpy as np
+import logging
 
 class RegressionLossEvaluator:
     num_evaluations: int
@@ -17,40 +18,38 @@ class RegressionLossEvaluator:
         self.contact_forces_weight = contact_forces_weight
 
         self.num_evaluations = 0
-        self.sum_loss = 0.0
         self.sum_timesteps = 0
-        self.sum_contact_forces_N_error = np.zeros(6)
+        self.sum_contact_forces_N_error = np.zeros((1,6))
         self.forces = []
 
     def __call__(self, outputs: Dict[str, torch.Tensor], labels: Dict[str, torch.Tensor]) -> torch.Tensor:
         # Compute the loss
-        loss = self.contact_forces_weight * torch.mean((outputs[OutputDataKeys.CONTACT_FORCES] - labels[OutputDataKeys.CONTACT_FORCES]) ** 2)
-
+        loss = self.contact_forces_weight * torch.sum((outputs[OutputDataKeys.CONTACT_FORCES] - labels[OutputDataKeys.CONTACT_FORCES]) ** 2, dim=0, keepdim=True)
         # Keep track of various performance metrics to report
         with torch.no_grad():
             self.num_evaluations += 1
             timesteps = outputs[OutputDataKeys.CONTACT_FORCES].shape[0]
             self.sum_timesteps += timesteps
-            self.sum_loss += loss.item()
-            self.sum_contact_forces_N_error += torch.sum((outputs[OutputDataKeys.CONTACT_FORCES] - labels[OutputDataKeys.CONTACT_FORCES]) ** 2, dim=0).numpy()
+            self.sum_contact_forces_N_error += loss.numpy()
             self.forces.append(labels[OutputDataKeys.CONTACT_FORCES].numpy())
-        return loss
+        return torch.sum(loss) / timesteps
 
     def print_report(self):
-        print('\tLoss: ' + str(self.sum_loss / self.num_evaluations))
+        logging.info(f'\tLoss: {np.sqrt(np.sum(self.sum_contact_forces_N_error) / self.sum_timesteps)}')
         self.forces = np.abs(np.concatenate(self.forces))
-        print('\tContact force avg N error (per axis), foot 1: ' +
-              str(np.sqrt(self.sum_contact_forces_N_error[:3] / self.sum_timesteps)), f"max={np.max(self.forces, axis=0)}, min={np.min(self.forces, axis=0)}, mean={np.mean(self.forces, axis=0)}")
-        print('\tContact force avg N error (per axis), foot 2: ' +
-              str(np.sqrt(self.sum_contact_forces_N_error[3:] / self.sum_timesteps)))
+        logging.info(f"max={np.max(self.forces, axis=0)}, min={np.min(self.forces, axis=0)}, mean={np.mean(self.forces, axis=0)}")
+        logging.info('\tContact force avg N error (per axis), foot 1: ' +
+              str(np.sqrt(self.sum_contact_forces_N_error[:,:3] / self.sum_timesteps)))
+        logging.info('\tContact force avg N error (per axis), foot 2: ' +
+              str(np.sqrt(self.sum_contact_forces_N_error[:,3:] / self.sum_timesteps)))
 
         # Reset
         self.num_evaluations = 0
-        self.sum_loss = 0.0
+        # self.sum_loss = 0.0
         self.sum_timesteps = 0
-        self.sum_correct_foot_classifications = 0.0
-        self.sum_com_acc_mpss_error = np.zeros(3)
-        self.sum_contact_forces_N_error = np.zeros(6)
-        self.confusion_matrix = np.zeros((4,4), dtype=np.int64)
+        # self.sum_correct_foot_classifications = 0.0
+        # self.sum_com_acc_mpss_error = np.zeros(3)
+        self.sum_contact_forces_N_error = np.zeros((1,6))
+        # self.confusion_matrix = np.zeros((4,4), dtype=np.int64)
         self.forces = []
         pass
--- models/FeedForwardRegressionBaseline.py
+++ models/FeedForwardRegressionBaseline.py
@@ -20,7 +20,7 @@ class FeedForwardBaseline(nn.Module):
         # For input, we need each dof, for position and velocity and acceleration, for each frame in the window, and then also the COM acceleration for each frame in the window
         input_size = dofs * window_size 
         # For output, we have four foot-ground contact classes (foot 1, foot 2, both, neither)
-        output_size = 6
+        output_size = 1
 
         self.dropout1 = nn.Dropout(dropout_prob)
         self.fc1 = nn.Linear(input_size, hidden_size, dtype=torch.float32, device=device)
@@ -28,7 +28,7 @@ class FeedForwardBaseline(nn.Module):
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, output_size, dtype=torch.float32, device=device)
         
-    def forward(self, input: Dict[str, torch.Tensor]):
+    def forward(self, input: Dict[str, torch.Tensor]) -> torch.Tensor:
         # Get the position, velocity, and acceleration tensors
         flattened_poses = input[InputDataKeys.POS].flatten(start_dim=-2)
         # Actually run the forward pass
@@ -37,9 +37,35 @@ class FeedForwardBaseline(nn.Module):
         x = self.dropout2(x)
         x = self.relu(x)
         x = self.fc2(x)
+        
+        return x
+
+class GRFPredictor(nn.Module):
+    def __init__(self, dofs: int, window_size: int, hidden_size: int = 64, dropout_prob: float = 0.5, device: str = 'cpu'):
+        super(GRFPredictor, self).__init__()
+        self.grf = nn.ModuleList([FeedForwardBaseline(dofs, window_size, hidden_size, dropout_prob, device) for _ in range(6)])
+    
+    def forward(self, input: Dict[str, torch.Tensor])  -> torch.Tensor:
+        outputs = []
+        for i in range(6):
+            outputs.append(self.grf[i].forward(input))
+        output = torch.cat(outputs, dim=1)
+        return output
+    
+class DynamicsPredictor(nn.Module):
+    def __init__(self, dofs: int, window_size: int, hidden_size: int = 64, dropout_prob: float = 0.5, device: str = 'cpu'):
+        super(DynamicsPredictor, self).__init__()
+        self.grf_predictor = GRFPredictor(dofs, window_size, hidden_size, dropout_prob, device)
+        # self.com_acc_predictor = 
+    
+    def forward(self, input: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         # Now we need to split the output into the different components
         output_dict: Dict[str, torch.Tensor] = {}
-        # First, the contact predictions
-        output_dict[OutputDataKeys.CONTACT_FORCES] = x
+        output_dict[OutputDataKeys.CONTACT_FORCES] = self.grf_predictor(input)
         
         return output_dict
+
+if __name__ == "__main__":
+    print(FeedForwardBaseline(19, 5))
+    print(GRFPredictor(19, 5))
+    print(DynamicsPredictor(19, 5))
