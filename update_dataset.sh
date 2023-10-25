3736c59f09f60103dd9601301a7e95e288d8fd5d
Keenon Werling <keenonwerling@gmail.com>
2023-10-25 16:55:58 -0700

    Committing my uncommited changes to update_dataset.sh'

--- update_dataset.sh
+++ update_dataset.sh
@@ -10,10 +10,14 @@ echo "Download the datasets from Alan's account that have been standardized to t
 addb -d dev download --prefix "protected/us-west-2:e013a4d2-683d-48b9-bfe5-83a0305caf87" "protected/us-west-2:e013a4d2-683d-48b9-bfe5-83a0305caf87/.*\.b3d$"
 popd
 
-echo "Post-process the data to low-pass filter at 20 Hz and standardize at 100 Hz:"
-addb post-process raw processed --geometry-folder "./Geometry/" --only-dynamics True --sample-rate 100 --recompute-values True --root-history-len 10 --root-history-stride 3
+echo "Post-process the data to clean up common issues with CoP and finite-differenced accelerations, and standardize at 100 Hz:"
+addb post-process raw processed --geometry-folder "./Geometry/" --only-dynamics True --clean-up-noise True --sample-rate 100 --recompute-values True --root-history-len 10 --root-history-stride 3
 popd
 
-#rm -rf data/train
-#rm -rf data/dev
-#python3 create_splits.py
+rm -rf train
+rm -rf dev
+popd
+
+pushd src
+python3 main.py create-splits
+popd
