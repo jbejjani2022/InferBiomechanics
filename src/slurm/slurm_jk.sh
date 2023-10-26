f6e29754c60be4723624aafd788ddb6ca9999dcb
Janelle Kaneda <janellekaneda@gmail.com>
2023-10-26 14:42:17 -0700

    new slurm file and organization

---
+++ src/slurm/slurm_jk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=model_training_run
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000M
+#SBATCH --partition=owners
+
+ml python/3.9.0
+
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+python3 main.py train --model feedforward --checkpoint-dir "$GROUP_HOME/cvpr/checkpoint-$TIMESTAMP" --hidden-size 256 --learning-rate 0.01 --opt-type adagrad --dataset-home "$GROUP_HOME/data" --epochs 500
