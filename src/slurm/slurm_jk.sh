bad093a004392a7cbd5e5f8a81d2d25ee32b66c3
Janelle Kaneda <janellekaneda@gmail.com>
2023-10-28 13:23:12 -0700

    rerun from checkpoint

--- src/slurm/slurm_jk.sh
+++ src/slurm/slurm_jk.sh
@@ -8,5 +8,6 @@
 
 ml python/3.9.0
 
-TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-python3 ../main.py train --model feedforward --checkpoint-dir "$GROUP_HOME/cvpr/checkpoint-$TIMESTAMP" --hidden-size 256 --learning-rate 0.01 --opt-type adagrad --dataset-home "$GROUP_HOME/data" --epochs 500
+#TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+#python3 ../main.py train --model feedforward --checkpoint-dir "$GROUP_HOME/cvpr/checkpoint-$TIMESTAMP" --hidden-size 256 --learning-rate 0.01 --opt-type adagrad --dataset-home "$GROUP_HOME/data" --epochs 500
+python3 ../main.py train --model feedforward --checkpoint-dir "$GROUP_HOME/cvpr/checkpoint-20231027022502" --hidden-size 64 --learning-rate 0.01 --opt-type adagrad --dataset-home "$GROUP_HOME/data" --epochs 500
