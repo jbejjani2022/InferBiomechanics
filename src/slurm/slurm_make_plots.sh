c58b2e9da3f0b01c8b8dd2484a8353cbe9b366f8
Janelle Kaneda <janellekaneda@gmail.com>
2023-10-30 14:36:25 -0700

    outline of analytical baseline edits; shell script for make plots

---
+++ src/slurm/slurm_make_plots.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=make_plots_run
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64000M
+#SBATCH --partition=owners
+
+ml python/3.9.0
+
+python3 /home/users/jkaneda/InferBiomechanics/src/main.py make-plots --data-path "/home/groups/delp/data" --out-path "/home/groups/delp/jkaneda/figures" --output-scatterplots False --output-errvfreq False
