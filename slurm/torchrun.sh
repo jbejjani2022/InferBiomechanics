cc3dceec16a79a89d5758e4758ce8c3130211f17
camilobrownpinilla <camilobrownpinilla@gmail.com>
2024-07-18 15:56:52 -0400

    small slurm script change:

--- slurm/torchrun.sh
+++ slurm/torchrun.sh
@@ -13,7 +13,7 @@ source activate paper_reimplementation_env
 cd ../src
 
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=${master_addr}-ib
+export MASTER_ADDR=$master_addr-ib
 
 torchrun --nnodes=2 --nproc_per_node=1 main.py train --model mdm --checkpoint-dir "../checkpoints/dffGsrV0/multinode_test" --opt-type adam --dataset-home "/n/holyscratch01/pslade_lab/AddBiomechanicsDataset/addb_dataset" --data-loading-workers 8 --dropout --dropout-prob 0.1 --epochs 10 --batch-size 256 --learning-rate 8e-4 --short
 
