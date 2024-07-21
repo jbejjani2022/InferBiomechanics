92a634df9e770618066c22d163193a158ade3afd
jbejjani2022 <jpbejjani@hotmail.com>
2024-07-21 16:38:34 -0400

    Remove find_unused_parameters=True flag in DDP constructor because no unused parameters are found and flag results in extra traversal reducing performance

--- src/cli/train.py
+++ src/cli/train.py
@@ -163,7 +163,7 @@ class TrainCommand(AbstractCommand):
                                device=device).to(device)
         
         # Wrap model in DDP class
-        model = DDP(model, device_ids=[device], output_device=device, find_unused_parameters=True)
+        model = DDP(model, device_ids=[device], output_device=device)
 
         params_to_optimize = filter(lambda p: p.requires_grad, model.parameters())
         if not list(params_to_optimize):
