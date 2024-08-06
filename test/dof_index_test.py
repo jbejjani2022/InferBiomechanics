af3b62ef95968f9496fd6ebfba06da5e31c8cfe1
jbejjani2022 <jpbejjani@hotmail.com>
2024-08-06 11:54:15 -0400

    remove tests

--- test/dof_index_test.py
+++
@@ -1,9 +0,0 @@
-from test.get_dataset import get_dataset
-
-short = False
-train_dataset, dev_dataset = get_dataset(short)
-print("TRAIN DATASET SKELETONS:")
-train_dataset.inspect_dof_indices()
-print('-' * 80)
-print("DEV DATASET SKELETONS:")
-dev_dataset.inspect_dof_indices()
