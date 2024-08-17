a2207878fad2f6d26792b7e937a705da00a99885
jbejjani2022 <jpbejjani@hotmail.com>
2024-08-17 12:58:24 -0400

    move output decoder layer to model init

--- src/models/MDM.py
+++ src/models/MDM.py
@@ -2,12 +2,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from data.AddBiomechanicsDataset import InputDataKeys, OutputDataKeys
+from data.AddBiomechanicsDataset import OutputDataKeys
 from typing import Dict
 
 class MDM(nn.Module):
-    def __init__(self, dofs: int, window_size=50, history_len=5, stride=1, latent_dim=256, ff_size=1024,
-                 num_layers=8, num_heads=4, dropout=0.1, activation='gelu', dtype=torch.float32, device='cpu'):
+    def __init__(self, dofs: int, window_size=50, stride=1, latent_dim=256, ff_size=1024,
+                 num_layers=8, num_heads=4, dropout=0.1, activation='gelu', dtype=torch.float32):
         super().__init__()
 
         self.ff_size = ff_size
@@ -16,11 +16,9 @@ class MDM(nn.Module):
         self.activation = activation
         self.dropout = dropout
         self.dtype = dtype
-        self.device = device
         self.dofs = dofs
 
-        # Compute the size of the input vector to the model, which is the concatenation
-        # of input keys
+        # Compute the size of the input vector to the model, which is the concatenation of input keys
         self.timestep_vector_dim = 2 + (dofs * 3) 
 
         # Output vector is 2 contact labels and three metrics per dof
@@ -29,8 +27,7 @@ class MDM(nn.Module):
         self.latent_dim = latent_dim
         self.num_output_frames = (window_size // stride)
 
-
-        self.input_process = InputProcess(self.timestep_vector_dim, self.latent_dim).to(device)
+        self.input_process = InputProcess(self.timestep_vector_dim, self.latent_dim)
         self.positional_encoding  = PositionalEncoding(self.latent_dim)
         seqTransEncoderLayer = nn.TransformerEncoderLayer(d_model=self.latent_dim,
                                                           nhead=self.num_heads,
@@ -40,8 +37,7 @@ class MDM(nn.Module):
         self.seqTransEncoder = nn.TransformerEncoder(seqTransEncoderLayer,
                                                  num_layers=self.num_layers)
         self.embed_timestep = TimestepEmbedder(self.latent_dim, self.positional_encoding)
-        self.temporal_embedding = TemporalEmbedding(
-            window_size, latent_dim, dtype=dtype)
+        self.output_decoder = nn.Linear(self.latent_dim, self.output_vector_dim, dtype=self.dtype)
         
     def parameters(self):
         return [p for name, p in self.named_parameters()]
@@ -51,9 +47,8 @@ class MDM(nn.Module):
         emb = self.embed_timestep(timesteps)
         xseq = torch.cat((emb, x), axis=0) #[frames+1, bs, feats]
         xseq = self.positional_encoding(xseq).to(self.dtype)
-        output = self.seqTransEncoder(xseq).to(self.device)[1:] #[frames, bs, feats]
-        output_decoder = nn.Linear(self.latent_dim, self.output_vector_dim, dtype=self.dtype, device=self.device)
-        output = output_decoder(output).permute(1, 2, 0) #[bs, feats, frames]
+        output = self.seqTransEncoder(xseq)[1:] #[frames, bs, feats]
+        output = self.output_decoder(output).permute(1, 2, 0) #[bs, feats, frames]
 
         # Split output into different components
         output_dict: Dict[str, torch.Tensor] = {}
